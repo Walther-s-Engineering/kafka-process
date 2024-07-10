4095201b8b4d74ab1918c5c6b97987ee8d5f197c
Alexander Walther <asushofman@gmail.com>
2024-07-10 19:32:54 +0300

    [kp-1] Enriched settings for connection settings model

--- kafka_process/data/models/connection.py
+++ kafka_process/data/models/connection.py
@@ -1,7 +1,7 @@
 import typing as t
 
 from pydantic.fields import Field, Required
-from tricky.typing import Integer, String
+from tricky.typing import Bool, Integer, String
 
 from . import FrozenBaseModel
 
@@ -17,8 +17,12 @@ class ConnectionSettings(FrozenBaseModel):
 
     sasl_mechanism: String = Field(Required, alias="sasl.mechanism")
     sasl_protocol: String = Field(Required, alias="security.protocol")
+
     max_poll_interval_ms: Integer = Field(alias="max.poll.interval.ms")
     session_timeout_ms: Integer = Field(alias="session.timeout.ms")
+
+    auto_offset_reset: String = Field(Required, alias="auto.offset.reset")
+    enable_auto_commit: Bool = Field(False, alias="enable.auto.commit")
 
     def dict(self, *args: t.Any, **kwargs: t.Any) -> t.Dict[String, t.Any]:
         kwargs.pop("by_alias", None)
